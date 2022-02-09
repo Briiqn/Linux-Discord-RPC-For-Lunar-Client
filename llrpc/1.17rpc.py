77154ed1bbebbaaaa05d505e7a5b5081e55ac806
Briiqn <82249811+Briiqn@users.noreply.github.com>
2022-02-08 20:21:58 -0500

    shit quick code, could of used cases but im to lazy

---
+++ llrpc/1.17rpc.py
@@ -0,0 +1,24 @@
+import tkinter
+import os
+from random import random
+from threading import Thread
+from time import sleep
+import time
+import random
+from time import mktime
+from pypresence import Presence
+import psutil
+import subprocess
+import platform
+root_tk = tkinter.Tk()  # create the Tk window like you normally do
+root_tk.geometry("0x0")
+"""
+https://discord.com/developers/applications/940028576407912450/rich-presence/assets
+"""
+while True:
+    client_id = '940028576407912450'
+    RPC = Presence(client_id)
+    RPC.connect()
+    start_time=time.time()
+    print(RPC.update(state="Launched With Lunar Client QT", details="State: Playing 1.17.1 ", large_image="large", buttons=[{"label": " My Github", "url": "https://github.com/Briiqn"}]))
+    time.sleep(10)
